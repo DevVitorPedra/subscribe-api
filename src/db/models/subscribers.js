b81b9bb8f79a99fb1ec5e7c64bcf0ca0b4ab9473
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2021-12-14 20:46:50 -0300

    types/sequelize

--- src/db/models/subscribers.js
+++ src/db/models/subscribers.js
@@ -3,11 +3,12 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../sequelize");
 
 const Subscribers = sequelize.define("Subscribers", {
-  name: DataTypes.STRING,
-  email: DataTypes.STRING,
-  phone: DataTypes.INTEGER,
+  name: {type:DataTypes.STRING},
+  email:{type:DataTypes.STRING,unique:true},
+  phone: {type:DataTypes.INTEGER},
   city:DataTypes.STRING,
-});
+},
+{timestamps:false});
 
 //create table if not exists...
 const init = async () => {
